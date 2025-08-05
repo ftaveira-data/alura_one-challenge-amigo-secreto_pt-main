7f1a330036f3ba1a24897ce6a8476ba12f515617
Francisco Taveira <ftaveira.dev@gmail.com>
2025-08-05 20:13:46 -0300

    feat: inclusao das functions adicionarAmigo e renderizarLista

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaNomes = [];
+
+function adicionarAmigo() {
+    let input = document.getElementById("amigo");
+    let nome = input.value.trim();
+
+    if (nome === "") {
+        alert("Digite um nome, o campo não pode ficar em branco!");
+        return;
+    }
+
+    listaNomes.push(nome);
+
+    input.value= "";
+
+    renderizarLista();
+}
+
+function renderizarLista() {
+    let ul = document.getElementById("listaAmigos");
+    ul.innerHTML = "";
+
+    listaNomes.forEach((nome) => {
+        let li = document.createElement("li");
+        li.textContent = nome;
+        ul.appendChild(li);
+    });
+}
+
